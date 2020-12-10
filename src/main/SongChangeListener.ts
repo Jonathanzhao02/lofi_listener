741c44494216136ecf38c81859b167d043557bd1
Jonathan Zhao <mac@Macs-MacBook-Pro.local>
2020-12-09 18:40:16 -0800

    disabled songchangelistener

--- src/main/SongChangeListener.ts
+++ src/main/SongChangeListener.ts
@@ -116,12 +116,15 @@ export default class SongChangeListener extends EventEmitter ***REMOVED***
 
     init(): void ***REMOVED***
         if (!fs.existsSync('resources')) fs.mkdirSync('resources');
-        extractLatestText(this.url).then(song => ***REMOVED***
+        this.currentSong = '???';
+        this.lastSong = '???';
+        this.songsPlayed = 0;
+        /*extractLatestText(this.url).then(song => ***REMOVED***
             this.currentSong = song;
             this.songsPlayed = 0;
             extractLatestGif(this.url);
             this.processId = setInterval(this.loop.bind(this), 5000);
-        ***REMOVED***);
+        ***REMOVED***);*/
     ***REMOVED***
 
     loop(): void ***REMOVED***
